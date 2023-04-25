b5047e157fe0dc74e352052d654d9dffb8b6ab5c
Raid7 Raid7 <thmrgb8@gmail.com>
2023-04-25 09:11:34 +0300

    t

--- API/Controllers/AuthenticationController.cs
+++ API/Controllers/AuthenticationController.cs
@@ -21,7 +21,7 @@ namespace API.Controllers
         private readonly ElibraryDbContext _context;
 
         public record UserData(string email, string password);
-        public record User(string username,int Id,string Role,string Department);
+        public record User(string username,int Id,string Role);
         public AuthenticationController(IConfiguration config, ElibraryDbContext context)
         {
             _config = config;
@@ -50,12 +50,12 @@ namespace API.Controllers
         }
         private string? GenToken(IUser user)
         {
-            string email = _context.ContactDetails.Where(x => x.ContactId == user.ContactId).FirstOrDefault().Email;
+            string email = _context.ContactDetails.Where(x => x.ContactId == user.ContactId).FirstOrDefault()!.Email!;
             var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetValue<string>("Authentication:SecretKey")));
             var SigningCreds = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
             List<Claim> claims = new();
             claims.Add(new(JwtRegisteredClaimNames.Sub, email));
-            claims.Add(new("Role", user.GetType().ToString()));
+            claims.Add(new("Role", user.GetType().Name));
             var token = new JwtSecurityToken(
                 _config.GetValue<string>("Authentication:Issuer"),
                 _config.GetValue<string>("Authentication:Audience"),
