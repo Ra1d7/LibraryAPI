e878cc16abb36cb34c98735bc881f2a0d4403146
Raid7 Raid7 <thmrgb8@gmail.com>
2023-04-25 09:30:44 +0300

    made the books controller async

--- API/Controllers/V2/BooksController.cs
+++ API/Controllers/V2/BooksController.cs
@@ -2,6 +2,8 @@
 using API.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,35 +23,69 @@ public class BooksController : ControllerBase
     // GET: api/<BooksController>
     [HttpGet]
     [AllowAnonymous]
-    public IEnumerable<Book> Get()
+    public async Task<List<Book>> Get()
     {
-        return _context.Books.ToList();
+        return await _context.Books.ToListAsync();
     }
 
     // GET api/<BooksController>/5
     [HttpGet("{bookid}")]
     [AllowAnonymous]
-    public IActionResult Get(int bookid)
+    public async Task<IActionResult> Get(int bookid)
     {
-        if(_context.Books.Where(x => x.BookId == bookid).Any()) { return Ok(_context.Books.FirstOrDefault(x => x.BookId == bookid)); }
+        if (await _context.Books.Where(x => x.BookId == bookid).AnyAsync()) 
+        {
+            return Ok(await _context.Books.FirstOrDefaultAsync(x => x.BookId == bookid)); 
+        }
         return NotFound("No book was found with that specefic Id");
     }
 
     // POST api/<BooksController>
     [HttpPost]
-    public void Post([FromBody] string value)
+    public async Task<IActionResult> Post([FromBody] Book book)
     {
+        try
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return Ok(book);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Couldn't Create book.\n\n {ex.Message}");
+        }
     }
 
     // PUT api/<BooksController>/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
+    [HttpPut("{book}")]
+    public async Task<IActionResult> Put([FromBody] Book book)
     {
+        try
+        {
+            _context.Books.Update(book);
+            await _context.SaveChangesAsync();
+            return Ok(book);
+
+        }catch (Exception ex)
+        {
+            return BadRequest($"Couldn't Update book \n\n {ex.Message}");
+        }
     }
 
     // DELETE api/<BooksController>/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
+    [HttpDelete("{bookid}")]
+    public async Task<IActionResult> Delete(int bookid)
     {
+        try
+        {
+            Book book = _context.Books.FirstOrDefault(x => x.BookId==bookid)!;
+            _context.Remove(book);
+            await _context.SaveChangesAsync();
+            return Ok(book);
+
+        }catch (Exception ex)
+        {
+            return BadRequest($"Couldn't Delete book with id {bookid} \n\n {ex.Message}");
+        }
     }
 }
